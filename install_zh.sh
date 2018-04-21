adcc5493a22501a58ddbfcc26201ced3347322fa
jinlong <jinlong@divemath.com>
2018-04-21 18:47:39 +0800

    fork plugin from github to gitee

---
+++ install_zh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "installing..."
+
+mv -f ~/.vim ~/.vim_old
+git clone https://github.com/killmymates/vimrc ~/.vim
+echo 'source ~/.vim/vimrcs/basic.vim
+source ~/.vim/vimrcs/plugins_zh.vim
+source ~/.vim/vimrcs/plugins_config.vim
+source ~/.vim/vimrcs/extended.vim' > ~/.vimrc
+vim +BundleInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py
+
+echo "Installed the Vim configuration successfully, Enjoy it ! :-)"
