5f5ef14772dc97cd0c14b40b684e39886988eb7b
jinlong <762293490@qq.com>
2018-05-06 21:19:20 +0800

    fix bug

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "installing..."
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim git cmake python3-dev libncurses5-dev
 ##Add HomeBrew support on  Mac OS
-if which brew >/dev/null;then
+elif which brew >/dev/null;then
     echo "You are using HomeBrew tool, I don't konw if it can work well"
     brew install vim git 
 fi
