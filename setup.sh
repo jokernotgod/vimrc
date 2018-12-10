eb501d0e020a3b286792c7e4fbcb5af716d806d9
MacPro <762293490@qq.com>
2018-12-10 16:53:19 +0800

    update setup while install on centos

--- setup.sh
+++ setup.sh
@@ -7,22 +7,31 @@ if which apt-get >/dev/null; then
 ##Add HomeBrew support on  Mac OS
 elif which brew >/dev/null;then
     echo "You are using HomeBrew tool, I don't konw if it can work well"
-    brew install vim git 
+    brew install vim git cmake
 fi
 # windows cmake
 # https://cmake.org/download/
 
-mv -f ~/.vim ~/.vim_old
+vim_path = '~/.vim'
+if [ ! -d "$vim_path"]; then
+    mv -f ~/.vim ~/.vim_old
+fi
 git clone https://github.com/formateddd/vimrc ~/.vim
 
-echo 'source ~/.vim/vimrcs/basic.vim
-source ~/.vim/vimrcs/plugins.vim
-source ~/.vim/vimrcs/plugins_config.vim
-source ~/.vim/vimrcs/extended.vim' > ~/.vimrc
+if which yum >/dev/null;then
+    echo 'source ~/.vim/vimrcs/basic.vim
+    source ~/.vim/vimrcs/extended.vim' > ~/.vimrc
+else:
+    echo 'source ~/.vim/vimrcs/basic.vim
+    source ~/.vim/vimrcs/plugins.vim
+    source ~/.vim/vimrcs/plugins_config.vim
+    source ~/.vim/vimrcs/extended.vim' > ~/.vimrc
 
-vim +PlugInstall +qall
-echo "vim plugins install success"
+    vim +PlugInstall +qall
+    echo "vim plugins install success"
+fi
 
-#default python virtualenv
+
+# default python virtualenv
 pip install yapf autopep8
 echo "Installed the Vim configuration successfully, Enjoy it ! :-)"
